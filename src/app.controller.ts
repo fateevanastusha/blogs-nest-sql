948e5b23aa67884c75fc6a06de11cdd016306d56
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-05-05 23:24:19 +0300

    change delete all data status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Delete } from "@nestjs/common";
+import { Controller, Delete, Res } from "@nestjs/common";
 import { BlogsRepository } from "./blogs/blogs.repository";
 import { PostsRepository } from "./posts/posts.repository";
 import { UsersRepository } from "./users/users.repository";
+import { Response} from "express";
 
 @Controller()
 export class AppController {
   constructor(protected blogsRepository : BlogsRepository, protected postsRepository : PostsRepository, protected usersRepository : UsersRepository) {}
 
   @Delete('/testing/all-data')
-  async deleteAllData() {
+  async deleteAllData(@Res() res : Response) {
     await this.blogsRepository.deleteAllData();
     await this.postsRepository.deleteAllData();
     await this.usersRepository.deleteAllData();
+    res.sendStatus(204)
     return;
   }
 }
