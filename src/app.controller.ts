14008d43c47a11a253208f0b6c4889666bc064cf
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-05-11 17:53:37 +0300

    add delete all data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,16 +3,27 @@ import { BlogsRepository } from "./blogs/blogs.repository";
 import { PostsRepository } from "./posts/posts.repository";
 import { UsersRepository } from "./users/users.repository";
 import { Response} from "express";
+import { SecurityRepository } from "./security/security.repository";
+import { LikesRepository } from "./likes/likes.repository";
+import { CommentsRepository } from "./comments/comments.repository";
 
 @Controller()
 export class AppController {
-  constructor(protected blogsRepository : BlogsRepository, protected postsRepository : PostsRepository, protected usersRepository : UsersRepository) {}
+  constructor(protected blogsRepository : BlogsRepository,
+              protected postsRepository : PostsRepository,
+              protected usersRepository : UsersRepository,
+              protected securityRepository : SecurityRepository,
+              protected likesRepository : LikesRepository,
+              protected commentsRepository : CommentsRepository) {}
 
   @Delete('/testing/all-data')
   async deleteAllData(@Res() res : Response) {
     await this.blogsRepository.deleteAllData();
     await this.postsRepository.deleteAllData();
     await this.usersRepository.deleteAllData();
+    await this.securityRepository.deleteAllData();
+    await this.likesRepository.deleteAllData();
+    await this.commentsRepository.deleteAllData();
     res.sendStatus(204)
     return;
   }
