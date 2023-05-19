2681829ff7846fd53351a11bfe4b87373fdfde50
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-05-19 18:40:54 +0300

    add 404 error

--- src/security/security.controller.ts
+++ src/security/security.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Req, UseGuards } from "@nestjs/common";
+import { Controller, Delete, Get, NotFoundException, Param, Req, UseGuards } from "@nestjs/common";
 import { CheckForRefreshToken, CheckForSameUser } from "../auth.guard";
 import { SecurityService } from "./security.service";
 
@@ -9,18 +9,21 @@ export class SecurityController{
   @UseGuards(CheckForRefreshToken)
   @Get('/')
   async getSessions(@Req() req: any){
-    console.log('security ', req.cookies.refreshToken);
     return await this.securityService.getAllSessions(req.cookies.refreshToken)
   }
   @Delete()
   async deleteSessions(@Req() req: any){
-    return await this.securityService.deleteAllSessions(req.cookies.refreshToken)
+    const status : boolean = await this.securityService.deleteAllSessions(req.cookies.refreshToken)
+    if (!status) throw new NotFoundException();
+    return;
   }
   @UseGuards(CheckForRefreshToken)
   @UseGuards(CheckForSameUser)
   @Delete(':id')
   async deleteSession(@Req() req: any,
                       @Param('id') deviceId : string){
-  return await this.securityService.deleteOneSession(req.cookies.refreshToken)
+    const status : boolean = await this.securityService.deleteOneSession(req.cookies.refreshToken);
+    if (!status) throw new NotFoundException();
+    return;
   }
 }
