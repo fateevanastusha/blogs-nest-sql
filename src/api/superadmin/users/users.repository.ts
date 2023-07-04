e8762f33364c94b48ca2dde5e4c005b1aae75ed7
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-07-04 15:16:33 +0300

    debugging

--- src/api/superadmin/users/users.repository.ts
+++ src/api/superadmin/users/users.repository.ts
@@ -12,13 +12,13 @@ export class UsersRepository {
       count = await this.dataSource.query(`
       SELECT COUNT(*) AS "total"
       FROM public."Users"
-      WHERE "login" LIKE '%${searchLoginTerm}%' AND "email" LIKE '%${searchEmailTerm}%'
+      WHERE "login" LIKE '%${searchLoginTerm}%' OR "email" LIKE '%${searchEmailTerm}%'
     `);
     } else {
       count = await this.dataSource.query(`
       SELECT COUNT(*) AS "total"
       FROM public."Users"
-      WHERE "login" LIKE '%${searchLoginTerm}%' AND "email" LIKE '%${searchEmailTerm}%'AND "isBanned" = ${banStatus}
+      WHERE ("login" LIKE '%${searchLoginTerm}%' OR "email" LIKE '%${searchEmailTerm}%')AND "isBanned" = ${banStatus}
     `);
     }
     return Number(count[0].total);
