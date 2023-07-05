531b1c8ed52bcb3171560896e5690fdb08182ebd
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-07-05 13:15:09 +0300

    debugging

--- src/api/public/auth/auth.controller.ts
+++ src/api/public/auth/auth.controller.ts
@@ -33,7 +33,7 @@ export class AuthController {
       let token: AccessToken = {
         accessToken: tokenList.accessToken
       }
-      res.cookie('refreshToken', tokenList.refreshToken, {httpOnly: false, secure: false})
+      res.cookie('refreshToken', tokenList.refreshToken, {httpOnly: true, secure: true})
       res.send(token)
       return
     } throw new UnauthorizedException()
@@ -106,7 +106,7 @@ export class AuthController {
     const title = req.headers["user-agent"] || "unknown"
     const tokenList: TokenList = await this.authService.createNewToken(req.cookies.refreshToken, req.ip, title)
     let token: AccessToken = { accessToken: tokenList.accessToken }
-    res.cookie('refreshToken', tokenList.refreshToken, {httpOnly: false, secure: false})
+    res.cookie('refreshToken', tokenList.refreshToken, {httpOnly: true, secure: true})
     res.send(token)
   }
   @Get('/me')
